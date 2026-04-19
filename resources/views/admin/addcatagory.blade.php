63de28d71a4d6a981c032a573060dd85598e5a5a
Girija1912 <mandalgirijanandan@gmail.com>
2026-04-19 16:04:47 +0530

    add_category

---
+++ resources/views/admin/addcatagory.blade.php
@@ -0,0 +1,11 @@
+@extends('admin.maindesign')
+
+@section('add_catagory')
+<div class="container-fluid">
+    <form action="" method="POST">
+        <input type="text" name="catagory">
+        <input type="submit" name="submit" value="Add Catagory">
+    </form>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,20 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
     return view('index');
 })->name('index');
 
+
+
 Route::get('/dashboard', [UserController::class, 'index'])
-->middleware(['auth', 'verified'])->name('dashboard');
+    ->middleware(['auth', 'verified'])->name('dashboard');
 
-
+Route::middleware('auth', 'admin')->group(function () {
+    Route::get('/addcatagory', [AdminController::class, "addcatagory"])->name('admin.addcatagory');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -19,4 +24,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
